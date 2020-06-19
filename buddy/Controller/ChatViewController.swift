8d90975fbc0fdd9ab1f180c6d8a1c2d1a803dff8
Maggie Guan <maggieguan149@gmail.com>
2020-06-19 15:27:13 -0700

    up to chat

--- buddy/Controller/ChatViewController.swift
+++ buddy/Controller/ChatViewController.swift
@@ -7,45 +7,40 @@
 //
 
 import UIKit
+import Firebase
 
-class ChatViewController: UITableViewController {
+class ChatViewController: UIViewController {
     
-    var messageArray = [
-        "hey",
-        "hi",
-        "ho"
-    ]
+    var messageArray = [Message]()
+    
+//    var database = firebase.firestore()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+}
+    
     
 //MARK: - Tableview datasource methods
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return messageArray.count
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ChatCell", for: indexPath)
-        cell.textLabel?.text = messageArray[indexPath.row]
-        return cell
+
+    extension ChatViewController: UITableViewDataSource{
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return messageArray.count
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let message = messageArray[indexPath.row]
+            let cell = tableView.dequeueReusableCell(withIdentifier: "ChatCell", for: indexPath)
+            cell.textLabel?.text = message.text
+            return cell
+        }
     }
     
     
 //MARK: - Tableview delegate methods
 
-    
-}
+    extension ChatViewController: UITableViewDelegate{
+        
+    }
+
