7dd54c41dd4aa14316f453fe57120bcb110bf299
Maggie Guan <maggieguan149@gmail.com>
2020-06-19 22:54:34 -0700

    rewriting chat box code

--- buddy/Controller/ChatViewController.swift
+++ buddy/Controller/ChatViewController.swift
@@ -11,13 +11,68 @@ import Firebase
 
 class ChatViewController: UIViewController {
     
+    @IBOutlet var tableView: UITableView!
+    @IBOutlet var messageTextfield: UITextField!
+    
+    let db = Firestore.firestore()
+    
     var messageArray = [Message]()
     
 //    var database = firebase.firestore()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+        navigationItem.title = "Buddy"
+        navigationItem.hidesBackButton = true
+        
+        tableView.rowHeight = 65
+        
+        tableView.register(UINib(nibName: "ChatBubble", bundle: nil), forCellReuseIdentifier: "ChatCell")
+        
+        loadMessages()
     }
+    
+    func loadMessages() {
+        db.collection("message").order(by: "date").addSnapshotListener { (querySnapshot, error) in
+            if let e = error{
+                print("error retrieving data from firestore, \(e)")
+            } else {
+                self.messageArray = []
+                for document in querySnapshot!.documents {
+                    let data = document.data()
+                    if let messageBody = data["text"] as? String, let messageSender = data["sender"] as? String{
+                        let message = Message(text: messageBody, sender: messageSender)
+                        self.messageArray.append(message)
+                        
+                        self.tableView.reloadData()
+                        let indexPath = IndexPath(row: self.messageArray.count - 1, section: 0)
+                        self.tableView.scrollToRow(at: indexPath, at: .top, animated: false)
+                    }
+                }
+            }
+        }
+    }
+    
+    @IBAction func sendPressed(_ sender: UIButton) {
+        if let messageSender = Auth.auth().currentUser?.email, let messageBody = messageTextfield.text{
+            self.messageTextfield.text = ""
+            db.collection("message").addDocument(data: [
+                "sender": messageSender,
+                "text": messageBody,
+                "date": Date().timeIntervalSince1970
+            ])
+            { (error) in
+                if let e = error {
+                    print("There was an issue saving data to firestore, \(e)")
+                } else {
+                    print("Successfully saved data")
+                }
+            }
+        }
+    }
+    
     
 }
     
@@ -30,9 +85,16 @@ class ChatViewController: UIViewController {
         }
         
         func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            let message = messageArray[indexPath.row]
-            let cell = tableView.dequeueReusableCell(withIdentifier: "ChatCell", for: indexPath)
-            cell.textLabel?.text = message.text
+            let cell = tableView.dequeueReusableCell(withIdentifier: "ChatCell", for: indexPath) as! ChatBubble
+            let currentMessage = messageArray[indexPath.row]
+            if Auth.auth().currentUser?.email == currentMessage.sender{
+                cell.leftSender.isHidden = true
+                cell.sender.isHidden = false
+            } else {
+                cell.sender.isHidden = true
+                cell.leftSender.isHidden = false
+            }
+            cell.label.text = currentMessage.text
             return cell
         }
     }
